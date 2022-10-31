c42e908ba11f79eafca74fa6d85d2864b9fcd96f
yegnasivasai <yegnasivasai@gmail.com>
2022-10-31 14:54:30 +0530

    Vpc

---
+++ vpc/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_vpc" "test_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+  tags = {
+    Name = "test_vpc"
+  }
+}
