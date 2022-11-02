01d5431c7c1e4a499a480d669198b0b6b9521896
yegnasivasai <yegnasivasai@gmail.com>
2022-11-02 11:50:52 +0530

    Testing

--- task_8_terraform_integration/main.tf
+++ task_8_terraform_integration/main.tf
@@ -7,4 +7,14 @@ resource "aws_instance" "terraform" {
   instance_type             = var.instance_type
   associate_public_ip_address = var.associate_public_ip_address
   key_name                     = var.key_name
+
+  tags = {
+    Name = "test"
+    application-name = "webserver"
+    project = "Holcim"
+    environment = "dev"
+    project-lifecycle = "dev"
+    cost-center = "aws"
+    map-migrated = "asia"
+  }
 }
