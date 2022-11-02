d20f329a19538c7b2936599c81a4c67a85a5bb2a
yegnasivasai <yegnasivasai@gmail.com>
2022-11-02 11:20:14 +0530

    Testing

---
+++ task_8_terraform_integration/vars.tf
@@ -0,0 +1,21 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "ami" {
+  default = "ami-08c40ec9ead489470"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "associate_public_ip_address" {
+  default = "true"
+}
+
+variable "key_name" {
+  default = "automation"
+}
+
+
